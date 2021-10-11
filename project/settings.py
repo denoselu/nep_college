ebb84d2f6d77d7fbf9f3deb9f8e8921f3285454e
denoselu <88776620+denoselu@users.noreply.github.com>
2021-10-11 12:24:19 +0300

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -174,7 +174,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'denoselu@gmail.com'
+EMAIL_HOST_USER = 'citcmombasaserver@gmail'
 EMAIL_HOST_PASSWORD = 'manage.py'
 ACCOUNT_EMAIL_VERIFICATION='none'
 
